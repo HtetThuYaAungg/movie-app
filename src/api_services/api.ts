99f6d98f20eeb3cf94d1f2edbadcdf731deb4cc8
HtetThuYaAungg <htetthuya.htya@gmail.com>
2024-04-08 12:44:12 +0630

    [ENHANCEMENT] update api end point

--- src/api_services/api.ts
+++ src/api_services/api.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 
 const apiInstance: AxiosInstance = axios.create({
-  baseURL: "http://192.168.98.254:3000" ,
+  baseURL: "https://movie-app-backend-v6g9.onrender.com" ,
   timeout: 180000, // 3 mins
   headers: {
     "Content-Type": "application/json",
